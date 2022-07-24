78ca48ea6d9bb1ebaddfde5a064c02af2aa9add7
Jason Hegyessy <jason@hegyessy.com>
2022-07-24 15:00:08 -0700

    resolve conflicts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { parse } from "https://deno.land/std@0.149.0/encoding/yaml.ts";
 
